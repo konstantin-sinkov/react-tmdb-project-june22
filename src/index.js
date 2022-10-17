943812920cd654f46b86566debe2d15bb6cad8cb
konstantin-sinkov <sinkov.ks@gmail.com>
2022-10-17 12:26:54 +0300

    start redux setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = store;
+
 root.render(
     <App />
 );
---
+++ src/redux/slices/movies.slice.js
@@ -0,0 +1,21 @@
+import {crea} from '@redux'
+
+const initialState = {
+    movies: [],
+    // genres: [],
+    currentMovie: null,
+    currentPage: null,
+    loading: false,
+    nightMode: false
+}
+
+const getMovies =
+
+const moviesSlice = createSlice({
+    name: 'moviesSlice',
+    initialState,
+    reducers: {
+    
+    }
+})
+
